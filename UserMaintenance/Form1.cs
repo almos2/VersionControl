fbb0ef1e9542308b6285aa9e47e2f35970ea03dc
Almos Hild <gyorgy.hild@stud.uni-corvinus.hu>
2019-11-25 19:06:14 +0100

    Add export function

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,12 +21,13 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.export;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
             listBox1.DataSource = users;
             
-
-
+            
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -38,6 +40,25 @@ namespace UserMaintenance
             };
             users.Add(u);
 
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName))
+                {
+                    foreach (var u in users)
+                    {
+                        sw.Write(u.ID);
+                        sw.Write(";");
+                        sw.Write(u.FullName);
+                        sw.WriteLine();
+                    }
+                }
+            }
         }
     }
 }
--- UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/Resource1.Designer.cs
@@ -70,6 +70,15 @@ namespace UserMaintenance {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Fájlba írás.
+        /// </summary>
+        internal static string export {
+            get {
+                return ResourceManager.GetString("export", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to TeljesNév.
         /// </summary>
         internal static string FullName {
